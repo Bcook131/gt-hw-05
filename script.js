3e8ca853e6a69bd6110e8ad2885f3f1a3c78df95
Brian Cook <bcrazorrider@gmail.com>
2020-06-15 22:12:41 -0400

    adding all files to the repo

---
+++ script.js
@@ -0,0 +1,90 @@
+let calendarDay = {
+    "8 AM": "",
+    "9 AM": "",
+    "10 AM": "",
+    "11 AM": "",
+    "12 AM": "",
+    "1 PM": "",
+    "2 PM": "",
+    "3 PM": "",
+    "4 PM": "",
+    "5 PM": "",
+};
+
+$(document).ready(function(){
+    if(!localStorage.getItem('calendarDay')) {
+        updateCalendarTasks(calendarDay);
+    } else {
+        updateCalendarTasks(JSON.parse(localStorage.getItem('calendarDay')));
+    }
+})
+
+$('#currentDate p').text(moment().format('dddd') + ", " + moment().format('MMMM Do YYYY, h:mm:ss a'));
+
+let counter = 1;
+for (const property in calendarDay) {
+    let textInfo = "#text-info" + counter;
+    $(textInfo).text(calendarDay[property]);
+    let timeId = ".hour" + counter;
+    let currentHour =  moment().hour();
+    let timeString = $(timeId).text();
+    let timeNumber = hourNumberFromHourString(timeString);
+    if (timeNumber < currentHour) {
+        $(textInfo).addClass("past");
+    } else if (timeNumber > currentHour) {
+        $(textInfo).addClass("future");
+    } else {
+        $(textInfo).addClass("present");
+    }
+    counter ++;
+}
+$("button").click(function() {
+    value = $(this).siblings("textarea").val();
+    hourString = $(this).siblings("div").text();
+
+    saveSchedule(hourString, value);
+});
+
+function hourNumberFromHourString(hourString) {
+    switch(hourString) {
+      case "8 AM": return 8;
+      case "9 AM": return 9;
+      case "10 AM": return 10;
+      case "11 AM": return 11;
+      case "12 PM": return 12;
+      case "1 PM": return 13;
+      case "2 PM": return 14;
+      case "3 PM": return 15;
+      case "4 PM": return 16;
+      case "5 PM": return 17;
+    }
+}
+function loadCorrectDataset() {
+    result = localStorage.getItem('calendarDay')
+    return (result ? result : calendarDay);
+}
+
+function initializeLocalStorage() {
+    localStorage.setItem('calendarDay', JSON.stringify(calendarDay));
+};
+
+function saveToLocalStorage(dayObj) {
+    localStorage.setItem('calendarDay', JSON.stringify(dayObj));
+}
+function saveSchedule(hourString, val) {
+    if(!localStorage.getItem('calendarDay')) {
+        initializeLocalStorage();
+    }
+    let workHours = JSON.parse(localStorage.getItem('calendarDay'));
+    workHours[hourString] = val
+
+  saveToLocalStorage(workHours);
+}
+
+
+function updateCalendarTasks(dayObject) {
+    $(".row").each(function(index) {
+        let res = $(this).children("div");
+        $(this).children("textarea").text(dayObject[res.text()]);
+    })
+}
